1788c610bdcfb3d0c757c190a6631525481e837f
Rajat Kashyap <rajatkashyap@apisero.com>
2021-08-16 16:51:06 +0530

    1st commit from cmd

---
+++ Fib.java
@@ -0,0 +1,27 @@
+class Fib{
+
+public static void main(String args[]){
+
+int number=7;
+
+int arr[]=new int[number];
+
+try{
+arr[0]=0;
+arr[1]=1;
+arr[2]=1;
+}
+catch(Exception ex){
+System.out.println("Array index out of BOUND exception\nYou have to enter the number greater than 2");
+}
+
+//manipulating
+for(int i=3;i<number;i++){
+arr[i]=arr[i-1]+arr[i-2];
+}
+
+//displaying
+System.out.println("Your series till "+number);
+for(int i:arr)
+System.out.print(i+" ");
+}}
