2293c00c01176a05c44d9482446b725fcd2cebd4
Rajat Kashyap <rajatkashyap@apisero.com>
2021-08-16 17:57:07 +0530

    3rd commit from cmd

--- Fib.java
+++ Fib.java
@@ -4,7 +4,7 @@ class Fib{
 public static void main(String args[])throws Exception{
 BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 
-int number=Integer.parseInt(%JOB_PARAM_1%);
+int number=Integer.parseInt(System.getProperty("param1"));
 
 int arr[]=new int[number];
 
